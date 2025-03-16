c90333a25a02cfc014d090d99015fea5835d2c57
Marcin Copik <mcopik@gmail.com>
2025-03-16 19:48:55 +0100

    [xaas] Add enum feature for IR type

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,36 +2,36 @@ import pytest
 import yaml
 from typing import Dict, Tuple
 
-from xaas.config import Config
+from xaas.config import XaaSConfig
 
 
 @pytest.fixture
-def resources() -> Tuple[Config, Dict]:
-    with open(Config.DEFAULT_CONFIGURATION, "r") as f:
-        return Config(), yaml.safe_load(f)
+def resources() -> Tuple[XaaSConfig, Dict]:
+    with open(XaaSConfig.DEFAULT_CONFIGURATION, "r") as f:
+        return XaaSConfig(), yaml.safe_load(f)
 
 
-def test_file_not_found(resources: Tuple[Config, Dict]):
+def test_file_not_found(resources: Tuple[XaaSConfig, Dict]):
     with pytest.raises(FileNotFoundError):
-        Config().initialize("nonexistent_file.yaml")
+        XaaSConfig().initialize("nonexistent_file.yaml")
 
 
-def test_singleton_pattern(resources: Tuple[Config, Dict]):
-    config = Config()
+def test_singleton_pattern(resources: Tuple[XaaSConfig, Dict]):
+    config = XaaSConfig()
 
     assert resources[0] == config
 
 
-def test_load_config(resources: Tuple[Config, Dict]):
-    config = Config()
-    config.initialize(Config.DEFAULT_CONFIGURATION)
+def test_load_config(resources: Tuple[XaaSConfig, Dict]):
+    config = XaaSConfig()
+    config.initialize(XaaSConfig.DEFAULT_CONFIGURATION)
 
     # check also singleton property
     assert resources[0].docker_repository == resources[1]["docker_repository"]
-    assert resources[0].ir_type == resources[1]["ir_type"]
+    assert resources[0].ir_type.value == resources[1]["ir_type"]
     assert resources[0].parallelism_level == resources[1]["parallelism_level"]
 
 
-def test_initialization_once(resources: Tuple[Config, Dict]):
+def test_initialization_once(resources: Tuple[XaaSConfig, Dict]):
     with pytest.raises(RuntimeError):
         resources[0].initialize("config.yaml")
